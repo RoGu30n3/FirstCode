5e0fcc5d747c1c440759dda06010cccc4592c9f5
RoGu30n3 <86502475+RoGu30n3@users.noreply.github.com>
2021-07-27 17:19:56 -0500

    Add files via upload

---
+++ Hello World!.py
@@ -0,0 +1,8 @@
+# Below is my first Python code "Hello World!" Created by RoGu3 on 07/27/2021
+print ("Hello World!")
+name_1 = "My name is RoGu3."
+location = "I am from the United States."
+likes = "I am really enjoying learning to code in Python!"
+print (name_1)
+print (location)
+print (likes)
